1c4c7dc74bee2361cb821046057c4cf44ab2d300
FranShott1 <167144173+FranShott1@users.noreply.github.com>
2025-10-29 21:21:30 -0300

    Change server port to 5000 and update listen method

--- boilerplate-project-sudoku-solver-main/server.js
+++ boilerplate-project-sudoku-solver-main/server.js
@@ -36,21 +36,24 @@ app.use(function(req, res, next) {
 });
 
 //Start our server and tests!
-const PORT = process.env.PORT || 3000
-app.listen(PORT, function () {
-  console.log("Listening on port " + PORT);
-  // process.env.NODE_ENV='test'
-  if (process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch (error) {
-        console.log('Tests are not valid:');
-        console.error(error);
-      }
-    }, 1500);
-  }
-});
+const PORT = process.env.PORT || 5000
+
+if (require.main === module) {
+  app.listen(PORT, '0.0.0.0', function () {
+    console.log("Listening on port " + PORT);
+    // process.env.NODE_ENV='test'
+    if (process.env.NODE_ENV==='test') {
+      console.log('Running Tests...');
+      setTimeout(function () {
+        try {
+          runner.run();
+        } catch (error) {
+          console.log('Tests are not valid:');
+          console.error(error);
+        }
+      }, 1500);
+    }
+  });
+}
 
 module.exports = app; // for testing
